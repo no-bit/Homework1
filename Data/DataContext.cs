4d06ae45c882700720a1a9f82bbea368c4461731
no-bit <sinanburakfiliz@gmail.com>
2022-09-08 18:00:23 +0300

    'commit'

---
+++ Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperations.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Location> Locations { get; set; }
+    }
+}
